bd065573257bf58c5637a223e240c5fbc207b65e
Nusret-Ali-Kok <nusretkok@aol.com>
2025-07-13 14:48:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+import json
+import os
 
 app = Flask(__name__)
 
+def load_experiences():
+    experience_folder = "experiences"
+    experiences = []
+
+    for filename in os.listdir(experience_folder):
+        if filename.endswith(".json"):
+            with open(os.path.join(experience_folder, filename), "r") as f:
+                experience = json.load(f)
+                experiences.append(experience)
+
+    return experiences
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    experiences = load_experiences()
+    return render_template("index.html", experiences=experiences)
 
 if __name__ == "__main__":
     app.run(debug=True)
